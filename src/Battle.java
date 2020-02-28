ea297cf5966c8df61d2786b681b003096d7634df
sshaj <sshajeenth@gmail.com>
2020-02-28 10:34:16 -0500

    Added Basic Battle Functionality

---
+++ src/Battle.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Battle {
+	
+	
+	public static boolean enemybattle(Player player, Monster enemy) {
+		System.out.printf("You have encountered a %s%n", enemy);
+		int playertick = 0, monstertick = 0;
+		
+		
+		while (player.getChp() > 0 && enemy.getChp() > 0) {
+			playertick += player.getSpd();
+			monstertick += enemy.getSpd();
+			
+			if (playertick > 1000) {
+				playertick -= 1000;
+				playermove(player, enemy);
+			}
+			
+			if (monstertick > 1000) {
+				monstertick -= 1000;
+				monstermove(player, enemy);
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	
+	public static void playermove(Player player, Monster enemy) {
+		
+
+		int action = 0;
+		String actin;
+		while (action > 4 || action < 1) {
+			System.out.printf("What do you want to do?%n1. Attack\t2. Skills%n3. Items\t4. Run%n%n");
+			
+			actin = GameStart.input.next();
+			
+			if (GameStart.isInteger(actin)) {
+				action = Integer.parseInt(actin);
+			} else {
+				action = 0;
+			}
+			
+			if (action == 1) {
+				player.attack();
+			} else if (action == 2) {
+				GameStart.NotInYet();
+			} else if (action == 3) {
+				GameStart.NotInYet();
+			} else if (action == 4) {
+				player.setChp(0);
+				System.out.printf("You ran from the %s", enemy);
+			} else {
+				System.out.println("Invalid action.");
+			}
+		}
+	}
+	
+	public static void monstermove(Player player, Monster enemy) {
+		int enemymove = GameStart.rand.nextInt(10);
+		
+		if (enemymove == 9 && enemy.specattack2()) { 
+			return;
+		} else if (enemymove >= 6 && enemy.specattack1()) {
+			return;
+		} else {
+			enemy.attack();
+		}
+	}
+}
--- src/GameStart.java
+++ src/GameStart.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GameStart {
 	
 	public static Scanner input = new Scanner(System.in);
+	public static Random rand = new Random();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,17 +12,26 @@ public class GameStart {
 		System.out.printf("Player %s, Level %d%n", player.getName(), player.getLevel());
 		Goblin gobbo = new Goblin();
 		System.out.printf("%s, HP %s%n",gobbo, gobbo.getMaxhp());
-		gobbo.attack();
-		player.attack();
-		player.setEqWeapon(new Weapon("Godsword", "You unleash the wrath of the heavens themselves upon your target.", 9999));
-		gobbo.specattack1();
-		System.out.printf("You equip the %s%n", player.getEqWeapon());
-		player.attack();
-		System.out.println("Holy fucking shit you killed Gobbo");
+		Battle.enemybattle(player, gobbo);
 		
 		
 		
 		
 	}
-
+	
+	public static boolean isInteger(String input) {
+		if (input == null) {
+			return false;
+		}
+		try {
+			int i = Integer.parseInt(input);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static void NotInYet() {
+		System.out.println("Not implemented yet");
+	}
 }
