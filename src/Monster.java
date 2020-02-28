bd8d5a919b4369b77f0a09a4214f0f3630de7403
sshaj <sshajeenth@gmail.com>
2020-02-28 11:20:47 -0500

    Moved Goblin class into Monster

--- src/Monster.java
+++ src/Monster.java
@@ -73,3 +73,29 @@ public class Monster {
 	
 	
 }
+
+//Goblin Monster
+class Goblin extends Monster {
+	
+	public Goblin() {
+		super(5, 3, 3, 3);
+	}
+	
+	public boolean attack(Player player) {
+		System.out.println("The Goblin Attacks");
+		
+		int damage = getAtk() - player.defend();
+		System.out.printf("It deals %d damage%n", damage);
+		player.setChp(player.getChp() - damage);
+		System.out.printf("You have %d health left.%n", player.getChp());
+
+		return true;
+	}
+	
+	public boolean specattack1(Player player) {
+		System.out.println("The Goblin taunts you and calls your mother a whore.");
+		return true;
+	}
+	
+	
+}
